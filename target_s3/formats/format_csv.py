e0b9db93fa9a50bba6b365a54e0822d2c5e6039a
crowemi <crowemi@hotmail.com>
2022-10-31 14:05:30 -0700

    added FormatBase to csv

--- target_s3/formats/format_csv.py
+++ target_s3/formats/format_csv.py
@@ -1,4 +1,7 @@
-class FormatCsv():
+from target_s3.formats.format_base import FormatBase
+
+
+class FormatCsv(FormatBase):
     def __init__(self) -> None:
         super().__init__()
         raise NotImplementedError
