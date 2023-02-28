c5cc0093dfe8469968aeab3e8514e065126c9714
crowemi <crowemi@hotmail.com>
2023-02-28 11:06:50 -0800

    added json encoder

--- target_s3/formats/format_json.py
+++ target_s3/formats/format_json.py
@@ -1,10 +1,24 @@
-import json
+from json import dumps, JSONEncoder
+from datetime import datetime
+
+from bson import ObjectId
+
 from target_s3.formats.format_base import FormatBase
+
+
+class JsonSerialize(JSONEncoder):
+    def default(self, obj: any) -> any:
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        else:
+            raise TypeError(f"Type {type(obj)} not serializable")
 
 
 class FormatJson(FormatBase):
     def __init__(self, config, context) -> None:
-        super().__init__(config, context, 'json')
+        super().__init__(config, context, "json")
         pass
 
     def _prepare_records(self):
@@ -13,8 +27,8 @@ class FormatJson(FormatBase):
         return super()._prepare_records()
 
     def _write(self) -> None:
-        return super()._write(json.dumps(self.records))
+        return super()._write(dumps(self.records, cls=JsonSerialize))
 
     def run(self) -> None:
         # use default behavior, no additional run steps needed
-        return super().run(self.context['records'])
+        return super().run(self.context["records"])
