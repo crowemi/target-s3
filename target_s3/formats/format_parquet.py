7f7610a092b539b763fee5c67d8d324d3f8fe48f
crowemi <crowemi@hotmail.com>
2023-03-01 11:51:54 -0800

    account for localhost sso

--- target_s3/formats/format_parquet.py
+++ target_s3/formats/format_parquet.py
@@ -7,16 +7,32 @@ from target_s3.formats.format_base import FormatBase
 
 class FormatParquet(FormatBase):
     def __init__(self, config, context) -> None:
-        super().__init__(config, context, 'parquet')
-        self.create_filesystem()
+        super().__init__(config, context, "parquet")
+        self.create_filesystem(
+            config.get("aws_profile_name", None),
+            config.get("aws_region", None),
+        )
 
-    def create_filesystem(self, aws_region: str = None) -> None:
+    def create_filesystem(
+        self,
+        aws_profile: str = None,
+        aws_region: str = None,
+    ) -> None:
         """Creates a pyarrow FileSystem object for accessing S3."""
         aws_region = self.aws_region if aws_region is None else aws_region
         try:
-            self.file_system = fs.S3FileSystem(region=aws_region)
+            if self.session:
+                self.file_system = fs.S3FileSystem(
+                    access_key=self.session.get_credentials().access_key,
+                    secret_key=self.session.get_credentials().secret_key,
+                    session_token=self.session.get_credentials().token,
+                )
+            else:
+                self.file_system = fs.S3FileSystem(
+                    region=aws_region,
+                )
         except Exception as e:
-            self.logger.error('Failed to create parquet file system.')
+            self.logger.error("Failed to create parquet file system.")
             self.logger.error(e)
             raise e
 
@@ -26,9 +42,11 @@ class FormatParquet(FormatBase):
             fields = set()
             for d in self.records:
                 fields = fields.union(d.keys())
-            dataframe = pyarrow.table({f: [row.get(f) for row in self.records] for f in fields})
+            dataframe = pyarrow.table(
+                {f: [row.get(f) for row in self.records] for f in fields}
+            )
         except Exception as e:
-            self.logger.error('Failed to create parquet dataframe.')
+            self.logger.error("Failed to create parquet dataframe.")
             self.logger.error(e)
             raise e
 
@@ -44,13 +62,13 @@ class FormatParquet(FormatBase):
             ParquetWriter(
                 f"{self.fully_qualified_key}.{self.extension}",
                 df.schema,
-                compression='gzip',  # TODO: support multiple compression types
+                compression="gzip",  # TODO: support multiple compression types
                 filesystem=self.file_system,
             ).write_table(df)
         except Exception as e:
-            self.logger.error('Failed to write parquet file to S3.')
+            self.logger.error("Failed to write parquet file to S3.")
             raise e
 
     def run(self) -> None:
         # use default behavior, no additional run steps needed
-        return super().run(self.context['records'])
+        return super().run(self.context["records"])
