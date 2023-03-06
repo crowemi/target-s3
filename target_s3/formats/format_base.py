975bfc7eff5a305e44d752975891e19c05aff685
crowemi <crowemi@hotmail.com>
2023-03-06 08:58:54 -0800

    rebase main

--- target_s3/formats/format_base.py
+++ target_s3/formats/format_base.py
@@ -5,57 +5,67 @@ import collections
 import logging
 from datetime import datetime
 from abc import ABCMeta, abstractmethod
+
+from boto3 import Session
 from smart_open import open
 
 
 LOGGER = logging.getLogger("target-s3")
 DATE_GRAIN = {
-    'year': 7,
-    'month': 6,
-    'day': 5,
-    'hour': 4,
-    'minute': 3,
-    'second': 2,
-    'microsecond': 1
+    "year": 7,
+    "month": 6,
+    "day": 5,
+    "hour": 4,
+    "minute": 3,
+    "second": 2,
+    "microsecond": 1,
 }
 COMPRESSION = {}
 
 
 def format_type_factory(object_type_class, *pargs, **kargs):
-    """ A factory for creating ObjectTypes. """
+    """A factory for creating ObjectTypes."""
     return object_type_class(*pargs, **kargs)
 
 
 class FormatBase(metaclass=ABCMeta):
 
-    """ This is the object type base class """
+    """This is the object type base class"""
+
     def __init__(self, config: dict, context: dict, extension: str) -> None:
         # TODO: perhaps we should do some scrubbing here?
         self.config = config
         self.context = context
         self.extension = extension
-        self.compression = 'gz'  # TODO: need a list of compatible compression types
+        self.compression = "gz"  # TODO: need a list of compatible compression types
 
-        self.aws_region = config.get('aws_region')  # required
-        self.bucket = config.get('bucket')  # required
-        self.prefix = config.get('prefix', None)
-        self.logger = context['logger']
-        self.stream_name_path_override = config.get('stream_name_path_override', None)
+        self.stream_name_path_override = config.get("stream_name_path_override", None)
 
+        self.bucket = config.get("bucket")  # required
+        self.session = Session(
+            region_name=config.get("aws_region"),
+            profile_name=config.get("aws_profile_name", None),
+        )
+        self.prefix = config.get("prefix", None)
+        self.logger = context["logger"]
         self.fully_qualified_key = self.create_key()
         self.logger.info(f"key: {self.fully_qualified_key}")
 
     @abstractmethod
     def _write(self, contents: str = None) -> None:
-        """ Execute the write to S3. (default) """
+        """Execute the write to S3. (default)"""
         # TODO: create dynamic cloud
         # TODO: is there a better way to handle write contents ?
-        with open(f"s3://{self.fully_qualified_key}.{self.extension}.{self.compression}", "w") as f:
+        with open(
+            f"s3://{self.fully_qualified_key}.{self.extension}.{self.compression}",
+            "w",
+            transport_params={"client": self.session.client("s3")},
+        ) as f:
             f.write(contents)
 
     @abstractmethod
     def run(self, records) -> None:
-        """ Execute the steps for preparing/writing records to S3. (default) """
+        """Execute the steps for preparing/writing records to S3. (default)"""
         self.records = records
         # prepare records for writing
         self._prepare_records()
@@ -64,46 +74,51 @@ class FormatBase(metaclass=ABCMeta):
 
     @abstractmethod
     def _prepare_records(self) -> None:
-        """ Execute record prep. (default) """
-        if self.config.get('flatten_records', None):
+        """Execute record prep. (default)"""
+        if self.config.get("flatten_records", None):
             # flatten records
-            self.records = list(map(lambda record: self.flatten_record(record), self.records))
-
+            self.records = list(
+                map(lambda record: self.flatten_record(record), self.records)
+            )
 
     def create_key(self) -> str:
-        batch_start = self.context['batch_start_time']
-        stream_name = self.context['stream_name'] if self.stream_name_path_override is None else self.stream_name_path_override
+        batch_start = self.context["batch_start_time"]
+        stream_name = (
+            self.context["stream_name"]
+            if self.stream_name_path_override is None
+            else self.stream_name_path_override
+        )
         folder_path = f"{self.bucket}/{self.prefix}/{stream_name}/"
-        file_name = ''
-        if self.config['append_date_to_prefix']:
-            grain = DATE_GRAIN[self.config['append_date_to_prefix_grain'].lower()]
+        file_name = ""
+        if self.config["append_date_to_prefix"]:
+            grain = DATE_GRAIN[self.config["append_date_to_prefix_grain"].lower()]
             folder_path += self.create_folder_structure(batch_start, grain)
-        if self.config['append_date_to_filename']:
-            grain = DATE_GRAIN[self.config['append_date_to_filename_grain'].lower()]
+        if self.config["append_date_to_filename"]:
+            grain = DATE_GRAIN[self.config["append_date_to_filename_grain"].lower()]
             file_name += f"{self.create_file_structure(batch_start, grain)}"
 
         return f"{folder_path}{file_name}"
 
     def create_folder_structure(self, batch_start: datetime, grain: int) -> str:
-        ret = ''
-        ret += f"{batch_start.year}/" if grain <= 7 else ''
-        ret += f"{batch_start.month:02}/" if grain <= 6 else ''
-        ret += f"{batch_start.day:02}/" if grain <= 5 else ''
-        ret += f"{batch_start.hour:02}/" if grain <= 4 else ''
-        ret += f"{batch_start.minute:02}/" if grain <= 3 else ''
-        ret += f"{batch_start.second:02}/" if grain <= 4 else ''
-        ret += f"{batch_start.microsecond}/" if grain <= 1 else ''
+        ret = ""
+        ret += f"{batch_start.year}/" if grain <= 7 else ""
+        ret += f"{batch_start.month:02}/" if grain <= 6 else ""
+        ret += f"{batch_start.day:02}/" if grain <= 5 else ""
+        ret += f"{batch_start.hour:02}/" if grain <= 4 else ""
+        ret += f"{batch_start.minute:02}/" if grain <= 3 else ""
+        ret += f"{batch_start.second:02}/" if grain <= 4 else ""
+        ret += f"{batch_start.microsecond}/" if grain <= 1 else ""
         return ret
 
     def create_file_structure(self, batch_start: datetime, grain: int) -> str:
-        ret = ''
-        ret += f"{batch_start.year}" if grain <= 7 else ''
-        ret += f"{batch_start.month:02}" if grain <= 6 else ''
-        ret += f"{batch_start.day:02}" if grain <= 5 else ''
-        ret += f"-{batch_start.hour:02}" if grain <= 4 else ''
-        ret += f"{batch_start.minute:02}" if grain <= 3 else ''
-        ret += f"{batch_start.second:02}" if grain <= 4 else ''
-        ret += f"{batch_start.microsecond}" if grain <= 1 else ''
+        ret = ""
+        ret += f"{batch_start.year}" if grain <= 7 else ""
+        ret += f"{batch_start.month:02}" if grain <= 6 else ""
+        ret += f"{batch_start.day:02}" if grain <= 5 else ""
+        ret += f"-{batch_start.hour:02}" if grain <= 4 else ""
+        ret += f"{batch_start.minute:02}" if grain <= 3 else ""
+        ret += f"{batch_start.second:02}" if grain <= 4 else ""
+        ret += f"{batch_start.microsecond}" if grain <= 1 else ""
         return ret
 
     def flatten_key(self, k, parent_key, sep) -> str:
@@ -112,12 +127,16 @@ class FormatBase(metaclass=ABCMeta):
         full_key = parent_key + [k]
         inflected_key = [n for n in full_key]
         reducer_index = 0
-        while len(sep.join(inflected_key)) >= 255 and reducer_index < len(inflected_key):
+        while len(sep.join(inflected_key)) >= 255 and reducer_index < len(
+            inflected_key
+        ):
             reduced_key = re.sub(
                 r"[a-z]", "", inflection.camelize(inflected_key[reducer_index])
             )
             inflected_key[reducer_index] = (
-                reduced_key if len(reduced_key) > 1 else inflected_key[reducer_index][0:3]
+                reduced_key
+                if len(reduced_key) > 1
+                else inflected_key[reducer_index][0:3]
             ).lower()
             reducer_index += 1
 
--- target_s3/target.py
+++ target_s3/target.py
@@ -22,32 +22,34 @@ class Targets3(Target):
             "aws_access_key",
             th.StringType,
             secret=True,
-            description="The aws secret access key for auth to S3."
+            description="The aws secret access key for auth to S3.",
         ),
         th.Property(
             "aws_secret_access_key",
             th.StringType,
             secret=True,
             required=False,
-            description="The aws secret access key for auth to S3."
+            description="The aws secret access key for auth to S3.",
         ),
         th.Property(
             "aws_region",
             th.StringType,
             description="The aws region to target",
-            required=True
+            required=True,
+        ),
+        th.Property(
+            "aws_profile_name",
+            th.StringType,
+            description="The aws profile name used with SSO.",
+            required=False,
         ),
         th.Property(
             "bucket",
             th.StringType,
             description="The aws bucket to target.",
-            required=True
+            required=True,
         ),
-        th.Property(
-            "prefix",
-            th.StringType,
-            description="The prefix for the key."
-        ),
+        th.Property("prefix", th.StringType, description="The prefix for the key."),
         th.Property(
             "stream_name_path_override",
             th.StringType,
@@ -56,30 +58,30 @@ class Targets3(Target):
         th.Property(
             "append_date_to_prefix",
             th.BooleanType,
-            description="A flag to append the date to the key prefix."
+            description="A flag to append the date to the key prefix.",
         ),
         th.Property(
             "append_date_to_prefix_grain",
             th.StringType,
             description="The grain of the date to append to the prefix.",
-            allowed_values=DATE_GRAIN.keys()
+            allowed_values=DATE_GRAIN.keys(),
         ),
         th.Property(
             "append_date_to_filename",
             th.BooleanType,
-            description="A flag to append the date to the key filename."
+            description="A flag to append the date to the key filename.",
         ),
         th.Property(
             "append_date_to_filename_grain",
             th.StringType,
             description="The grain of the date to append to the filename.",
-            allowed_values=DATE_GRAIN.keys()
+            allowed_values=DATE_GRAIN.keys(),
         ),
         th.Property(
             "format_type",
             th.StringType,
             description="The format of the storage object.",
-            allowed_values=sinks.FORMAT_TYPE.keys()
+            allowed_values=sinks.FORMAT_TYPE.keys(),
         ),
         th.Property(
             "flatten_records",
@@ -91,10 +93,7 @@ class Targets3(Target):
             th.BooleanType,
             description="A flag indictating to set dytpe to string.",
         ),
-        th.Property(
-            "stream_maps",
-            th.ObjectType()
-        ),
+        th.Property("stream_maps", th.ObjectType()),
     ).to_dict()
 
     default_sink_class = s3Sink
